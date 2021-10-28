d692e721f27b0b321a3d0d8552a308d3a61d0d39
Lê Phạm Thái An <us.thaian@gmail.com>
2021-10-28 19:44:46 +0700

    Hic
    Reset may trang nen day file nay len

---
+++ Bojii/app/src/main/java/com/example/bojii/Screen/VerifyPhoneNo.java
@@ -0,0 +1,17 @@
+package com.example.bojii.Screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bojii.R;
+
+public class VerifyPhoneNo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_phone_no);
+
+    }
+}
